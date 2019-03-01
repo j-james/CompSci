e726989018b41c3e88e8dce988a0dad31d125b22
j-james <j-james@users.noreply.github.com>
2019-03-01 09:49:56 -0800

    Finish A5

--- PixLab/src/PictureTester.java
+++ PixLab/src/PictureTester.java
@@ -1,10 +1,10 @@
 /**
  * This class contains class (static) methods
- * that will help you test the Picture class 
+ * that will help you test the Picture class
  * methods.  Uncomment the methods and the code
  * in the main to test.
- * 
- * @author Barbara Ericson 
+ *
+ * @author Barbara Ericson
  */
 public class PictureTester
 {
@@ -16,7 +16,7 @@ public class PictureTester
     beach.zeroBlue();
     beach.explore();
   }
-  
+
   /** Method to test mirrorVertical */
   public static void testMirrorVertical()
   {
@@ -25,7 +25,7 @@ public class PictureTester
     caterpillar.mirrorVertical();
     caterpillar.explore();
   }
-  
+
   /** Method to test mirrorTemple */
   public static void testMirrorTemple()
   {
@@ -34,7 +34,7 @@ public class PictureTester
     temple.mirrorTemple();
     temple.explore();
   }
-  
+
   /** Method to test the collage method */
   public static void testCollage()
   {
@@ -42,7 +42,7 @@ public class PictureTester
     canvas.createCollage();
     canvas.explore();
   }
-  
+
   /** Method to test edgeDetection */
   public static void testEdgeDetection()
   {
@@ -50,7 +50,31 @@ public class PictureTester
     swan.edgeDetection(10);
     swan.explore();
   }
-  
+
+  public static void testKeepOnlyBlue()
+  {
+    Picture dropbear = new Picture("someFunPicture.jpg");
+    dropbear.explore();
+    dropbear.keepOnlyBlue();
+    dropbear.explore();
+  }
+
+  public static void testNegate()
+  {
+    Picture dropbear = new Picture("someFunPicture.jpg");
+    dropbear.explore();
+    dropbear.negate();
+    dropbear.explore();
+  }
+
+  public static void testGreyscale()
+  {
+    Picture dropbear = new Picture("someFunPicture.jpg");
+    dropbear.explore();
+    dropbear.greyscale();
+    dropbear.explore();
+
+  }
   /** Main method for testing.  Every class can have a main
     * method in Java */
   public static void main(String[] args)
@@ -58,12 +82,12 @@ public class PictureTester
     // uncomment a call here to run a test
     // and comment out the ones you don't want
     // to run
-    testZeroBlue();
+    //testZeroBlue();
     //testKeepOnlyBlue();
     //testKeepOnlyRed();
     //testKeepOnlyGreen();
     //testNegate();
-    //testGrayscale();
+    //testGreyscale();
     //testFixUnderwater();
     //testMirrorVertical();
     //testMirrorTemple();
